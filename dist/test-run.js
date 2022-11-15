bb3da63825c89c2714f1a09929529b4bc23fd14f
Faddah Wolf <my_biz@me.com>
2022-11-15 15:05:03 -0800

    Adjustments to ansi-colors require statement and adjusting ansi-colors styling syntax.

--- dist/test-run.js
+++ dist/test-run.js
@@ -1,15 +1,15 @@
 const c = require('ansi-colors');
-console.log(c.blue(c.italic(`is this working?`)));
+console.log(c.blue.italic(`is this working?`));
 //the "expect" comparison
 const expect = (value) => {
     this.value = value;
     const toEqual = (compareValue) => {
-        return !!(this.value === compareValue);
+        return this.value === compareValue;
     };
     return { toEqual };
 };
 // the "it" test
 const it = (testString, callback) => {
-    console.log(c.green(`${testString}: ${callback() ? c.bold(`PASSED.`) : c.red(c.bold(`FAILED!`))}`));
+    console.log(c.green(`${testString}: ${callback() ? c.bold(`PASSED.`) : c.red.bold(`FAILED`)}`));
 };
 it('adds two numbers', () => expect(1 + 1).toEqual(2));
--- src/test-run.ts
+++ src/test-run.ts
@@ -1,19 +1,19 @@
 const c = require('ansi-colors');
 
-console.log(c.blue(c.italic(`is this working?`)));
+console.log(c.blue.italic(`is this working?`));
 
 //the "expect" comparison
 const expect = (value: number): any => {
 	this.value = value;
 	const toEqual = (compareValue: number): boolean => {
-		return !!(this.value === compareValue);
+		return this.value === compareValue;
 	}
 	return { toEqual };
 }
 
 // the "it" test
 const it = (testString: string, callback: Function): void => {
-	console.log(c.green(`${testString}: ${callback() ? c.bold(`PASSED.`) : c.red(c.bold(`FAILED!`))}`));
+	console.log(c.green(`${testString}: ${callback() ? c.bold(`PASSED.`) : c.red.bold(`FAILED`)}`));
 };
 
 it('adds two numbers', (): void => expect(1 + 1).toEqual(2));
