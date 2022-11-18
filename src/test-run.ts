3feeef56e124442f205cc6746ce62f46d5ad3422
Faddah Wolf <my_biz@me.com>
2022-11-18 12:00:03 -0800

    Added toHaveLength array method test.

--- src/test-run.ts
+++ src/test-run.ts
@@ -3,11 +3,19 @@ const c = require('ansi-colors');
 console.log(c.blue.italic(`is this working?`));
 
 //the "expect" comparison
-const expect = (value: number): any => {
+const expect = (value: number | any[]): any => {
+	let testArray: any[];
+	Array.isArray(value) ? testArray = value.slice() : testArray = null;
 	const toEqual = (compareValue: number): boolean => {
 		return value === compareValue;
 	};
-	return { toEqual };
+	const toHaveLength = (arrayLength: number): boolean => {
+		return testArray.length === arrayLength;
+	}
+	return {
+		toEqual,
+		toHaveLength
+	};
 }
 
 // the "it" test
@@ -15,4 +23,7 @@ const it = (testString: string, callback: Function): void => {
 	console.log(c.green(`${testString}: ${callback() ? c.bold(`PASSED.`) : c.red.bold(`FAILED!`)}`));
 };
 
+const arrTest: any[] = [1, 2, 3];
+
 it('adds two numbers', (): void => expect(1 + 1).toEqual(2));
+it('has the correct length', (): void => expect(arrTest).toHaveLength(3));
